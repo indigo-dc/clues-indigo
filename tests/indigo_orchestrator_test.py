a8fa54e71f8a5f505d36719167075e6a0087e931
alpegon <alpegon3@upvnet.upv.es>
2016-06-06 17:36:53 +0200

    Adde tests for task and Vmnode

--- tests/indigo_orchestrator_test.py
+++ tests/indigo_orchestrator_test.py
@@ -1,8 +1,36 @@
 import unittest
 from indigo_orchestrator import powermanager
-from mock import MagicMock
+from mock import MagicMock, patch
 
 class TestMesosPlugin(unittest.TestCase):
+
+    def test_powermanager_Task(self):
+        task = powermanager.Task(powermanager.POWER_ON, 'test')
+        assert task.nname == 'test'
+        assert task.operation == powermanager.POWER_ON
+        
+    def test_powermanager_Task_cmp(self):
+        task1 = powermanager.Task(powermanager.POWER_ON, 'test')
+        task2 = powermanager.Task(powermanager.POWER_ON, 'test')
+        task3 = powermanager.Task(powermanager.POWER_OFF, 'test')
+        assert task1.__cmp__(task2) == 0
+        assert task1.__cmp__(task3) == -1
+        
+    def test_powermanager_Task_str(self):
+        task1 = powermanager.Task(powermanager.POWER_OFF, 'test')
+        assert str(task1) == 'Power Off on test'
+        task2 = powermanager.Task(powermanager.POWER_ON, 'test')
+        assert str(task2) == 'Power On on test'        
+        
+    @patch('cpyutils.eventloop.now')        
+    def test_powermanager_VmNode(self, mock_timer):
+        mock_timer.return_value = 5
+        vm = powermanager.VM_Node(1)
+        assert vm.vm_id == 1
+        assert vm.timestamp_recovered == 0
+        assert vm.timestamp_created == 5
+        assert vm.timestamp_seen == 5
+
 
     def test_powermanager_power_on(self):
         mock_pm = MagicMock(powermanager)
