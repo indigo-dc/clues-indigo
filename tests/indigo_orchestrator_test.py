357b9ca816afdef55ab614e2d8612db623d47dac
micafer <micafer1@upv.es>
2016-06-07 10:12:22 +0200

    Add lifecycle test

--- tests/indigo_orchestrator_test.py
+++ tests/indigo_orchestrator_test.py
@@ -1,8 +1,9 @@
 import unittest
 import os
-import mock
 from indigo_orchestrator import powermanager
-from mock import MagicMock, patch
+from mock import MagicMock, Mock, patch
+
+from clueslib.node import Node, NodeInfo
 
 def read_file_as_string(file_name):
     tests_path = os.path.dirname(os.path.abspath(__file__))
@@ -64,12 +65,12 @@ class TestMesosPlugin(unittest.TestCase):
     def get_resources_page(self, page=0):
         return 200, read_file_as_string("test-files/orchestrator-resources-p%d.json" % page)
 
-    @mock.patch('cpyutils.eventloop.now')
-    @mock.patch('cpyutils.db.DB.create_from_string')
-    @mock.patch('indigo_orchestrator.powermanager._create_db')
-    @mock.patch('indigo_orchestrator.powermanager._load_pending_tasks')
-    @mock.patch('indigo_orchestrator.powermanager._load_mvs_seen')
-    @mock.patch('indigo_orchestrator.powermanager._get_resources_page')
+    @patch('cpyutils.eventloop.now')
+    @patch('cpyutils.db.DB.create_from_string')
+    @patch('indigo_orchestrator.powermanager._create_db')
+    @patch('indigo_orchestrator.powermanager._load_pending_tasks')
+    @patch('indigo_orchestrator.powermanager._load_mvs_seen')
+    @patch('indigo_orchestrator.powermanager._get_resources_page')
     def test_get_vms(self, get_resources_page, load_mvs_seen, load_pending_tasks, create_db, cpyutils_db_create, now):
         now.return_value = 1.0
         create_db.return_value = True
@@ -79,6 +80,40 @@ class TestMesosPlugin(unittest.TestCase):
         get_resources_page.side_effect = self.get_resources_page
         vms = powermanager()._get_vms()
         assert vms["vnode1"].timestamp_seen == 1.0
+        
+    @patch('cpyutils.eventloop.now')
+    @patch('cpyutils.db.DB.create_from_string')
+    @patch('indigo_orchestrator.powermanager._create_db')
+    @patch('indigo_orchestrator.powermanager._load_pending_tasks')
+    @patch('indigo_orchestrator.powermanager._load_mvs_seen')
+    @patch('indigo_orchestrator.powermanager._get_resources_page')
+    @patch('indigo_orchestrator.powermanager._get_deployment_status')
+    @patch('indigo_orchestrator.powermanager._process_pending_tasks')
+    def test_lifecycle(self, process_pending_tasks, get_deployment_status, get_resources_page, load_mvs_seen, 
+                       load_pending_tasks, create_db, cpyutils_db_create, now):
+        now.return_value = 1.0
+        create_db.return_value = True
+        load_mvs_seen.return_value = {'vnode1': powermanager.VM_Node('ee6a8510-974c-411c-b8ff-71bb133148eb')}
+        load_pending_tasks.return_value = []
+        cpyutils_db_create.return_value = None
+        get_resources_page.side_effect = self.get_resources_page
+        get_deployment_status.return_value = "CREATE_COMPLETE"
+        process_pending_tasks.return_value = True
+        pm = powermanager()
+        
+        pm._clues_daemon = MagicMock()
+        pm._db = MagicMock()
+        pm._db.sql_query = Mock(return_value=True)
+        monitoring_info = MagicMock()
+        node = Node("vnode1", 1, 1, 512, 512)
+        node.set_state(NodeInfo.OFF)
+        monitoring_info.nodelist = {node}
+        pm._clues_daemon.get_monitoring_info.return_value = monitoring_info 
+        now.return_value = 10000.0
+
+        pm.lifecycle()
+        
+        assert str(pm._pending_tasks[0]) == "Power Off on ee6a8510-974c-411c-b8ff-71bb133148eb"
 
 if __name__ == '__main__':
     unittest.main()
