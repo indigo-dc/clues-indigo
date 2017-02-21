54b801dde398df6b927f8e85ccef8b2d8f867fd4
micafer <micafer1@upv.es>
2017-02-21 12:52:33 +0100

    Remove unused import

--- indigo_orchestrator.py
+++ indigo_orchestrator.py
@@ -15,7 +15,6 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
-from platform import node
 '''
 Created on 26/1/2015
 
